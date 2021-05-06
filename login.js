8416545eb702e1edc53624cce9bd9a0738e80a6b
vkortikere <69329819+vkortikere@users.noreply.github.com>
2021-05-05 20:27:04 -0400

    Delete login.js

--- login.js
+++
@@ -1,7 +0,0 @@
-function login(){
-    user_name = document.getElementById("user_name").value;
-    localStorage.setItem("userName", user_name);
-    window.location = "chatPage.html";
-    console.log(user_name);
-    
-  }
