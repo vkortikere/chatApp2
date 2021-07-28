cdcdc956ec8628a50736fced2637e6b851e880ab
vkortikere <69329819+vkortikere@users.noreply.github.com>
2021-07-28 11:39:01 -0400

    Add files via upload

--- chatPage4/login.js
+++ chatPage4/login.js
@@ -2,7 +2,7 @@ function login(){
     user_name = document.getElementById("user_name").value;
     localStorage.setItem("user_name", user_name);
     console.log(user_name);
-    window.location = "chatPage.html";
+    window.location = "chatRoom.html";
  
     
   }
