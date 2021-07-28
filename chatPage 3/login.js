98d589a98d0c5c07a31b416d97ec900b4fe6c2ff
vkortikere <69329819+vkortikere@users.noreply.github.com>
2021-07-28 11:38:39 -0400

    Delete chatPage 3 directory

--- chatPage 3/login.js
+++
@@ -1,8 +0,0 @@
-function login(){
-    user_name = document.getElementById("user_name").value;
-    localStorage.setItem("user_name", user_name);
-    console.log(user_name);
-    window.location = "chatPage.html";
- 
-    
-  }
